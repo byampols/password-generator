3079df85b068838c564c5e7bc10912c8a434e038
unknown <byampols@alumni.cmu.edu>
2021-08-18 16:03:51 -0400

    added feature to choose types and confirm

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,75 @@
 // Assignment code here
-var characters = [["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
-                  ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
-                  ["0","1","2","3","4","5","6","7","8","9"],
-                  [" ","!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"]];
-
-var lowerCase = characters[0];
-var upperCase = characters[1];
-var numeric = characters[2];
-var special = characters[3];
+//define a global variable which contains objects including all sets of characters and their names as a descriptor
+var characters = [
+  {
+    name: "lower case",
+    set: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  },{
+    name: "uppercase",
+    set: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+  },{
+    name: "numeric",
+    set: ["0","1","2","3","4","5","6","7","8","9"]
+  },{
+    name: "special",
+    set: [" ","!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"]
+  }];
 
 //function that requests a length and returns n as a parsed int
 var getLength = function() {
+  //define min and max password lengths
+  var minLength = 8;
+  var maxLength = 128;
+  //ask for desired length
   var n = window.prompt("Please enter desired password length (8-128 characters).");
+  //turn length into an integer
   n = parseInt(n);
+  //if length is not an integer, or is out of the declared bounds, re-run the function
+  if (!Number.isInteger(n) || n > maxLength || n < minLength) {
+    window.alert("Please enter a valid response.");
+    n = getLength();
+  }
   return n;
+}
+
+//function that requests a type and returns an array of types and the names of the chosen types
+var getTypes = function() {
+  var types = [];
+  var typeNames = "";
+  //for each type of character
+  for (let i = 0; i < characters.length; i++) {
+    //check if the user wants the type of character
+    var nTypeConfirm = window.confirm("Would you like your password to contain " + characters[i].name + " characters?");
+    //if they do, add it to the types array
+    if (nTypeConfirm) {
+      types.push(characters[i].set);
+      typeNames += (characters[i].name + ", ")
+    }
+  }
+  typeNames = typeNames.substring(0, typeNames.length - 2);
+  return [types, typeNames];
 }
 
 //function that returns a random password, given proper inputs
 var generatePassword = function() {
+  //initialize local variables
   var password = "";
-  var types = characters;
-  //password criteria: length (8-128 characters), types (lowercase, uppercase, numeric, special), ensure at least 1 type
-  //is selected, and that each prompt is confirmed
-  var n = getLength();
-  while (!Number.isInteger(n) || n > 128 || n < 8) {
-    window.alert("Please enter a valid response.");
-    n = getLength();
+  var confirmLength = false;
+  var confirmTypes = false;
+  //while cancelled, acquire desired length, then confirm desired length
+  while (!confirmLength) {
+    var n = getLength();
+    confirmLength = window.confirm("The length of your password will be " + n + " characters.");
   }
+
+  //while cancelled, acquire desired types, then confirm types
+  while (!confirmTypes) {
+    window.alert("Please select which character types you would like to include in your password.");
+    var typeArray = getTypes();
+    confirmTypes = window.confirm("Your chosen types are: " + typeArray[1] + ".");
+  }
+  //once the types are confirmed, set the types
+  var types = typeArray[0];
   
   //for each unit of length of the password
   for (let i = 0; i < n; i++) {
